463fba63776e0bd9190fb67ece5fca1333c6e3f3
JoelStalin <ing.joelstalinmartinez@gmail.com>
2019-05-05 01:38:23 -0400

    klk

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    router.get('/', function (req, res) {
+        res.render('index', {
+            title: 'About Us' });
 });
 
 module.exports = router;
